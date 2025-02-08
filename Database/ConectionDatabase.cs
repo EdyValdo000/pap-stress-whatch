4ad6f6d8882d7480659981160b6ea56b0c57655a
EdyValdo000 <edivaldocoelho000@example.com>
2025-02-08 19:21:22 +0100

    Ajuste para criar as tabelas.

--- Database/ConectionDatabase.cs
+++ Database/ConectionDatabase.cs
@@ -1,4 +1,5 @@
-﻿using SQLite;
+﻿using pap.Model;
+using SQLite;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -6,43 +7,19 @@ namespace pap.Database
 {
     public class ConectionDatabase
     {
-        private readonly SQLiteAsyncConnection _database;
+        private readonly SQLiteAsyncConnection database;
 
         public ConectionDatabase()
         {
             var local = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "pap.db3");
-            _database = new SQLiteAsyncConnection(local);
-            _database.CreateTableAsync<Users>().Wait();
-        }
-
-        // Método para salvar um usuário (Create)
-        public Task<int> SaveUser(Users user)
-        {
-            return _database.InsertAsync(user);
-        }
-
-        // Método para obter todos os usuários (Read)
-        public Task<List<Users>> GetUsersAsync()
-        {
-            return _database.Table<Users>().ToListAsync();
-        }
-
-        // Método para obter um usuário por ID (Read)
-        public Task<Users> GetUserByIdAsync(int id)
-        {
-            return _database.Table<Users>().Where(u => u.Id == id).FirstOrDefaultAsync();
-        }
-
-        // Método para atualizar um usuário (Update)
-        public Task<int> UpdateUserAsync(Users user)
-        {
-            return _database.UpdateAsync(user);
-        }
-
-        // Método para deletar um usuário (Delete)
-        public Task<int> DeleteUserAsync(Users user)
-        {
-            return _database.DeleteAsync(user);
-        }
+            database = new SQLiteAsyncConnection(local);
+            
+            database.CreateTableAsync<User>().Wait();
+            database.CreateTableAsync<AIRecommendation>().Wait();
+            database.CreateTableAsync<GSRData>().Wait();
+            database.CreateTableAsync<HeartOximeterData>().Wait();
+            database.CreateTableAsync<MonitoringSession>().Wait();
+            database.CreateTableAsync<TemperatureData>().Wait();
+        }       
     }
 }
